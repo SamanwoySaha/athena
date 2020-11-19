24b567ce346ac98fc8be6114ce9648a2ccef35fd
SamanwoySaha <samanwoy.kuet@gmail.com>
2020-11-19 20:34:51 +0600

    added banner and team section

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -3,8 +3,23 @@ import './Banner.scss';
 
 const Banner = () => {
     return (
-        <div>
-            <h1>this is banner</h1>
+        <div id="banner">
+            <img className="img-fluid top-left d-sm-none d-md-block" src="https://i.imgur.com/7eV3zlp.png" alt="" />
+            <img className="img-fluid top-right d-sm-none d-md-block" src="https://i.imgur.com/gRZy8kO.png" alt="" />
+            <div className="container">
+                <div className="row d-flex justify-content-between align-items-center">
+                    <div className="col-sm-12 col-md-6 mb-5 pb-5">
+                        <h1 className="main-heading pb-4">Florence agency</h1>
+                        <p className="description pb-4 pr-5 mr-3">
+                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                        </p>
+                        <button id="main-btn">See Pricing</button>
+                    </div>
+                    <div className="col-sm-12 col-md-6">
+                        <img className="img-fluid" src="https://i.imgur.com/XFXZ46j.png" alt="" />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/TeamCard/TeamCard.js
+++ src/components/TeamCard/TeamCard.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './TeamCard.scss';
 
-const TeamCard = () => {
+const TeamCard = ({ plan }) => {
+    const { price, name, page, freeSupport } = plan;
+
     return (
-        <div>
-            <h1>this is a team card</h1>
+        <div className="col-sm-12 col-md-4">
+            <ul className="team-card description">
+                <li className="price">${price}</li>
+                <li>For {name}</li>
+                <li className="line"></li>
+                <li>Homepage</li>
+                <li>{page !== 0 ? page : 'No'} Inner Page</li>
+                <li>Asset File</li>
+                <li>Source File</li>
+                <li>Free Stock Photos</li>
+                <li>{freeSupport} Days Free Support</li>
+                <li>24/7 Support</li>
+                <button id="main-btn">Order Now</button>
+            </ul>
         </div>
     );
 };
